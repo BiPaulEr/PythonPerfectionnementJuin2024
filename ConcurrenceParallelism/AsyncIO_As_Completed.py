edb1fa3dae295e95d21148560a3a95517968d6e8
Paul-Ernest <PaulErMartin@yahoo.fr>
2024-06-26 16:13:10 +0200

    AsyncIO_As_Completed + correction de Exercice2

---
+++ ConcurrenceParallelism/AsyncIO_As_Completed.py
@@ -0,0 +1,20 @@
+import asyncio
+import time 
+import random
+
+async def worker(label):
+    timestosleep = random.randint(1, 10)
+    print(f"Worker {label} needs {timestosleep} to finish.")
+    await asyncio.sleep(timestosleep)  # Simulez un délai de réseau
+    result = random.randint(0, 100)  # Générez une température aléatoire
+    print(f"Worker {label} has ended")
+    return f"Worker {label} result is {result}"
+
+async def creation_de_toutes_les_taches():
+    tasks = (asyncio.create_task(worker(label)) for  label in ["A", "B", "C", "D"])
+    for task in asyncio.as_completed(tasks):
+        result = await task
+        print(f"Result received : {result}")
+
+    
+asyncio.run(creation_de_toutes_les_taches())
--- ConcurrenceParallelism/AsyncIO_Exerice_2.py
+++ ConcurrenceParallelism/AsyncIO_Exerice_2.py
@@ -12,7 +12,7 @@ async def creation_de_toutes_les_taches():
     cities = ["City1","City2","City3"]
     #create_task not required for gather
     #listdereponse = await asyncio.gather(*(asyncio.create_task(fetch_weather(city)) for city in cities)) 
-    listdereponse = await asyncio.gather(*(asyncio.create_task(fetch_weather(city)) for city in cities))
+    listdereponse = await asyncio.gather(*(fetch_weather(city) for city in cities))
     moyenne = sum([rapport_city["température"] for rapport_city in listdereponse]) / len(listdereponse)
     
     print("Resultat après gather")
